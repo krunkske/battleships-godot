4ea47c12f08c459250ebff4b24b8ed440dc32bcb
krunkske <123201489+krunkske@users.noreply.github.com>
2024-04-26 08:32:25 +0200

    added web platform

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,6 +5,12 @@ var last_hovered_cell
 var current_hovered_cell
 var current_hovered_board
 
+#not used yet but should prob do in the future
+const water_layer = 0
+const ships_layer = 1
+const  pins_layer = 2
+const preview_layer = 3
+
 func _ready():
 	#aLoad.you.global_position = (get_viewport().get_visible_rect().size/2) - Vector2(200,200) + get_viewport().get_visible_rect().size/5
 	#aLoad.opponent.global_position = (get_viewport().get_visible_rect().size/2) - Vector2(200,200) - get_viewport().get_visible_rect().size/5
@@ -12,10 +18,9 @@ func _ready():
 		self.get_node("AnimationPlayer" + str(i)).play("RESET")
 	current_hovered_board = $board1
 
-
 func _process(delta):
 	if aLoad.gameloop == "pregame":
-		preview(aLoad.yourTilemap.local_to_map(aLoad.yourTilemap.get_local_mouse_position()), 1)
+		preview(aLoad.yourTilemap.local_to_map(aLoad.yourTilemap.get_local_mouse_position()))
 		
 		if Input.is_action_just_pressed("LmouseButton"):
 			add_boat(aLoad.yourTilemap.local_to_map(aLoad.yourTilemap.get_local_mouse_position()))
@@ -48,34 +53,41 @@ func _process(delta):
 		if aLoad.gameloop == "pregame":
 			current_hovered_cell = aLoad.yourTilemap.local_to_map(aLoad.yourTilemap.get_local_mouse_position())
 			last_hovered_cell = current_hovered_cell
-			preview(current_hovered_cell, 1)
+			preview(current_hovered_cell)
 
-func preview(cell, num):
-
-	clear_layer(3, "you")
+#previews a boat on the preview layer
+#params: cell to start at
+func preview(cell):
+	#clears the preview layer
+	clear_layer(preview_layer, "you")
 	match aLoad.selected_boat:
 		"small":
-			small_boat(cell, 3)
+			small_boat(cell, preview_layer)
 		
 		"medium":
-			medium_boat(cell, 3)
+			medium_boat(cell, preview_layer)
 		
 		"large":
-			large_boat(cell, 3)
+			large_boat(cell, preview_layer)
 		
 		"extra_large":
-			extra_large_boat(cell, 3)
+			extra_large_boat(cell, preview_layer)
 
+#clears a specified layer on a specified tilemap
+#TODO check and change, works but bugs
 func clear_layer(layer, tilemap):
+	var tileMap = null
 	if tilemap == "you":
-		for kol in range(0,10):
-			for row in range(0, 10):
-				aLoad.yourTilemap.set_cell(layer, Vector2i(kol, row), -1)
+		tileMap = aLoad.yourTilemap
 	elif tilemap == "opponent":
-		for kol in range(0,10):
-			for row in range(0, 10):
-				current_hovered_board.set_cell(layer, Vector2i(kol, row), -1)
+		tileMap = current_hovered_board
+	else:
+		tileMap = tilemap
+	for kol in range(0,10):
+		for row in range(0, 10):
+			tileMap.set_cell(layer, Vector2i(kol, row), -1)
 
+#clears all of the layers from all of the tilemaps
 func clear_all_layers():
 	for user in aLoad.players:
 		for kol in range(0,10):
@@ -83,6 +95,7 @@ func clear_all_layers():
 				user[2].set_cell(1, Vector2i(kol, row), -1)
 				user[2].set_cell(2, Vector2i(kol, row), -1)
 
+#adds a specified boat to your tilemap and changes the boats_placed array
 func add_boat(cell):
 	match aLoad.selected_boat:
 		"small":
@@ -109,6 +122,7 @@ func add_boat(cell):
 					aLoad.boats_placed[3] += 1
 					aLoad.boat_select_GUI.set_value("extra_large", 0)
 
+#removes a specified boat from your tilemap and changes the boats_placed array
 func remove_boat(cell):
 	var count = 0
 	for i in aLoad.boats:
@@ -133,66 +147,17 @@ func remove_boat(cell):
 				
 		count += 1
 
+#checks if a cell on your board is occupied and if the cell is wihin the bounds of your board
 func cell_valid(cell):
 	if aLoad.yourTilemap.get_cell_source_id(1,cell) == -1 and cell_within_bounds(cell):
 		return true
 
+#checks if a cell is within the bounds of a 10x10 board
 func cell_within_bounds(cell):
 	if cell.x >= 0 and cell.x < 10 and cell.y >= 0 and cell.y < 10:
 		return true
-#deprecated
-func place_boat(cell, length, preview):
-	var modifer = 0
-	var layer = 1
-	if preview:
-		layer = 3
-		
-	match length:
-		2:
-			modifer = 0
-		3:
-			modifer = -1
-		4:
-			modifer = -1
-		5:
-			modifer = -2
-	
-	var boatCells = []
-	
-	if direction == "HORIZONTAL":
-		for i in length:
-			if not cell_valid(Vector2i(cell.x + (i + modifer), cell.y)):
-				return false
-		
-		for i in length:
-			if i == 0:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(0,0))
-			elif i == length - 1:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(2,0))
-			else:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(1,0))
-			
-			boatCells.append(Vector2i(cell.x + (i + modifer), cell.y))
-		
-	elif direction == "VERTICAL":
-		for i in length:
-			if not cell_valid(Vector2i(cell.x, cell.y + (i + modifer))):
-				return false
-		
-		for i in length:
-			if i == 0:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(0,1))
-			elif i == length - 1:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(2,1))
-			else:
-				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(1,1))
-			
-			boatCells.append(Vector2i(cell.x, cell.y + (i + modifer)))
-	if not preview:
-		aLoad.boats.append(boatCells)
-	
-	return true
 
+#functions to add boats to your tilemap at the specified location and layer
 func small_boat(cell, layer):
 	var boatcells = []
 	if direction == "HORIZONTAL":
@@ -297,48 +262,21 @@ func extra_large_boat(cell, layer):
 		aLoad.boats.append(boatcells)
 	return true
 
+#gets the list of a player by the given id of that player
+#TODO see if neccesary
 func get_list_from_id(id):
 	for i in aLoad.players:
 		if i[1] == id:
 			return i
 
+#gets the list of a player by the given board of that player
+#TODO see if neccesary
 func get_list_from_board(board):
 	for i in aLoad.players:
 		if i[2] == board:
 			return i
 
-@rpc("authority", "call_local", "reliable")
-func start_pregame():
-	aLoad.gameloop = "pregame"
-	aLoad.GUI.get_node("boat_select_menu").set_visible(true)
-	aLoad.top_gui.get_node("Label").set_text("Place your boats")
-	for i in len(aLoad.players):
-		aLoad.players[i][2] = aLoad.boards[i]
-		if aLoad.players[i][1] == multiplayer.get_unique_id():
-			aLoad.yourPosition = i
-	aLoad.yourTilemap = aLoad.players[aLoad.yourPosition][2]
-	
-	for i in len(aLoad.players):
-		print("played board" + str(i+1) + "_in")
-		self.get_node("AnimationPlayer" + str(i+1)).play("board_in")
-	
-	print("playerList " + str(multiplayer.get_unique_id()) + " : " + str(aLoad.players))
-
-
-@rpc("authority", "call_local", "reliable")
-func start_game():
-	aLoad.user_turn = 1
-	aLoad.gameloop = "game"
-	ready_all()
-	if aLoad.user_turn == multiplayer.get_unique_id():
-		aLoad.top_gui.get_node("Label").set_text("It's your turn")
-	else:
-		aLoad.top_gui.get_node("Label").set_text("It's turn")
-
-func ready_all():
-	for i in len(aLoad.players):
-		aLoad.players[i][3] = true
-
+#returns the next user who is allowed to place a boat by the previous users id
 func next_user_turn(id):
 	var valid_players = []
 	for i in aLoad.players:
@@ -356,6 +294,47 @@ func next_user_turn(id):
 			next = true
 
 	return aLoad.players[0][1]
+
+
+#MULTIPLAYER FUNCTIONS
+
+
+#starts the pregame
+#called by the server when all players joined and are ready to start
+@rpc("authority", "call_local", "reliable")
+func start_pregame():
+	aLoad.gameloop = "pregame"
+	aLoad.GUI.get_node("boat_select_menu").set_visible(true)
+	aLoad.top_gui.get_node("Label").set_text("Place your boats")
+	#fills in the missing variables in the playerlist like your position in the list and the players board
+	for i in len(aLoad.players):
+		aLoad.players[i][2] = aLoad.boards[i]
+		if aLoad.players[i][1] == multiplayer.get_unique_id():
+			aLoad.yourPosition = i
+	aLoad.yourTilemap = aLoad.players[aLoad.yourPosition][2]
+	
+	#plays the animations for the boards fading in
+	for i in len(aLoad.players):
+		print("played board" + str(i+1) + "_in")
+		self.get_node("AnimationPlayer" + str(i+1)).play("board_in")
+	
+	print("playerList " + str(multiplayer.get_unique_id()) + " : " + str(aLoad.players))
+
+#starts the game
+#called by the server when everyone has placed its boats and is ready to start the gameloop
+@rpc("authority", "call_local", "reliable")
+func start_game():
+	aLoad.user_turn = 1
+	aLoad.gameloop = "game"
+	#makes sure that all of the ready bariables in the playerlist are ste to true bc we repurpose them later
+	for i in len(aLoad.players):
+		aLoad.players[i][3] = true
+	#host advantage by making him go first kinda dunno
+	if aLoad.user_turn == multiplayer.get_unique_id():
+		aLoad.top_gui.get_node("Label").set_text("It's your turn")
+	else:
+		aLoad.top_gui.get_node("Label").set_text("It's turn")
+
 
 #TODO this code has to be checked and refactored again. It's a mess
 #DONE but still shit and i don't care
@@ -446,3 +425,57 @@ func mouse_board_3():
 func mouse_board_4():
 	if len(aLoad.players) >= 4:
 		current_hovered_board = aLoad.board4
+
+
+#deprecated
+func place_boat(cell, length, preview):
+	var modifer = 0
+	var layer = 1
+	if preview:
+		layer = 3
+		
+	match length:
+		2:
+			modifer = 0
+		3:
+			modifer = -1
+		4:
+			modifer = -1
+		5:
+			modifer = -2
+	
+	var boatCells = []
+	
+	if direction == "HORIZONTAL":
+		for i in length:
+			if not cell_valid(Vector2i(cell.x + (i + modifer), cell.y)):
+				return false
+		
+		for i in length:
+			if i == 0:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(0,0))
+			elif i == length - 1:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(2,0))
+			else:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x + (i + modifer), cell.y), 0, Vector2i(1,0))
+			
+			boatCells.append(Vector2i(cell.x + (i + modifer), cell.y))
+		
+	elif direction == "VERTICAL":
+		for i in length:
+			if not cell_valid(Vector2i(cell.x, cell.y + (i + modifer))):
+				return false
+		
+		for i in length:
+			if i == 0:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(0,1))
+			elif i == length - 1:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(2,1))
+			else:
+				aLoad.yourTilemap.set_cell(layer, Vector2i(cell.x, cell.y + (i + modifer)), 0, Vector2i(1,1))
+			
+			boatCells.append(Vector2i(cell.x, cell.y + (i + modifer)))
+	if not preview:
+		aLoad.boats.append(boatCells)
+	
+	return true
